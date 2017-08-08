331b2364e165070d5a01908e092e320267e131fc
Muhammad Muhajir <muhajirframe@gmail.com>
2017-08-08 08:20:07 +0700

    add index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,51 @@
 <template>
   <section class="container">
-    <div>
-      <h1 class="title">Title</h1>
-      <h2 class="subtitle">Subtitle</h2>
+    <div class="heading">
+      <h1 class="title">Makapala</h1>
+      <div class="subtitle">
+        <h2>Satu</h2>
+        <div class="timnyawa">
+          <h3 class="tim">Tim</h3>
+          <h3 class="nyawa">Nyawa</h3>
+        </div>
+      </div>
     </div>
   </section>
-</template>``
+</template>
+
+<style scoped>
+.container {
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+}
+
+.heading {
+  padding-left: 3rem;
+  line-height: 0;
+}
+
+.title {
+  font-size: 5rem;
+  font-weight: 300;
+}
+
+.subtitle {
+  font-size: 3rem;
+  display: block;
+  letter-spacing: 1rem;
+  h2 {
+    display: inline-block;
+    font-weight: 700;
+    font-size: 4rem;
+  }
+  .timnyawa {
+    display: inline-block;
+
+    line-height: 0rem;
+    font-size: 1rem;
+    font-weight: 300;
+  }
+}
+</style>
