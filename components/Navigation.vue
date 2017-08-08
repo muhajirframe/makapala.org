adf33dab00c80759e6387ca68fbb26eff4731f40
Muhammad Muhajir <muhajirframe@gmail.com>
2017-08-08 07:46:24 +0700

    add navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,14 +1,63 @@
 <template>
-  <nav class="navbar">
-    &#9776;
+  <nav class="navbar" @click="opened = !opened">
+    <div class="hamburger">
+      {{ menuIcon }}
+    </div>
+    <div class="overlay" v-if="opened">
+      <nuxt-link class="link" to='/'>Home</nuxt-link>
+      <nuxt-link class="link" to='/'>Sejarah</nuxt-link>
+      <nuxt-link class="link" to='/'>Prestasi</nuxt-link>
+      <nuxt-link class="link" to='/'>Anggota</nuxt-link>
+      <nuxt-link class="link" to='/'>Artikel</nuxt-link>
+    </div>
   </nav>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      opened: false
+    }
+  },
+  computed: {
+    menuIcon () {
+      return this.opened ? '⨯' : '☰'
+    }
+  }
+}
+</script>
+
 
 <style scoped>
 .navbar {
   position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.hamburger {
+  position: absolute;
+  width: 100%;
   right: 2rem;
   top: 2rem;
+
+  font-size: 2rem;
+  text-align: right;
+}
+
+.overlay {
+  height: 100vh;
+  padding: 3rem 0;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+.link {
+  text-align: center;
+  text-decoration: none;
   font-size: 2rem;
 }
 </style>
