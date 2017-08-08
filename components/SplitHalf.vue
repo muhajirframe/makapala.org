b8dadaf82cec5aaac2129fff8d676f6cbe234546
Muhammad Muhajir <muhajirframe@gmail.com>
2017-08-08 11:08:34 +0700

    extract SplitHalf comonent from sejarah page

---
+++ components/SplitHalf.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper">
+    <div class="title">
+      <h1>{{ title }}</h1>
+    </div>
+    <div class="text">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  max-height: 100vh;
+  height: 100%;
+  width: 100%;
+}
+.title {
+  text-align: center;
+  padding-top: 3rem;
+  padding-bottom: 1rem;
+  h1 {
+    font-size: 2rem;
+  }
+}
+.text {
+  overflow: scroll;
+  padding: 2rem 3rem;
+  flex: 1;
+  font-size: 1.5rem;
+  line-height: 2rem;
+}
+
+@media (min-width: 700px) {
+  .wrapper {
+    flex-direction: row;
+  }
+  .title{
+    padding: 0;
+    width: 40%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-bottom: 3rem;
+  }
+  .text {
+    font-size: 1rem;
+      letter-spacing: 0.1rem;
+  }
+}
+</style>
+ <script>
+ export default {
+   props: ['title']
+ }
+ </script>
--- pages/sejarah.vue
+++ pages/sejarah.vue
@@ -1,57 +1,19 @@
 <template>
-  <div class="wrapper">
-    <div class="title">
-      <h1>SEJARAH</h1>
-    </div>
-    <div class="text">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate tempora quis laborum voluptas dolorem voluptatibus perferendis autem quaerat expedita tenetur, in possimus maxime ducimus cumque quod harum, id omnis ratione.</p>
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, dolorem beatae doloremque aspernatur, soluta atque voluptatem ut veritatis delectus iure cumque minima est eaque autem harum ducimus repellendus blanditiis a doloribus sit. Quis consectetur aut necessitatibus saepe ullam, omnis animi?</p>
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, eligendi.</p>
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias accusamus numquam voluptatum animi nisi facilis labore, recusandae pariatur temporibus quisquam, voluptate id repellendus ab omnis explicabo ex similique, totam iste? Ducimus nobis reprehenderit, ullam illo possimus dolore. Fugit, culpa laboriosam, nam obcaecati cupiditate ipsum. Expedita, vero nihil sint placeat beatae, totam saepe odio minima cumque possimus doloribus. Quaerat, reiciendis, quis.</p>
-    </div>
-  </div>
+  <Split title="Sejarah">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis fuga ipsum optio possimus ad libero incidunt dolores eius, voluptatibus nulla! Facilis enim tempora unde aspernatur, blanditiis illo facere corporis vitae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, officiis!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias eos, eveniet similique rerum ad reprehenderit ducimus facere, vero aliquam sunt velit in accusantium inventore omnis porro at unde natus, necessitatibus.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex in corrupti beatae iure fuga, explicabo, molestiae perferendis deleniti pariatur, ratione nobis quibusdam cum, commodi a nihil repellat veniam velit libero! Reprehenderit ipsam natus nisi necessitatibus recusandae dolor laudantium, nemo ullam.</p>
+  </Split>
 </template>
 
-<style scoped>
-.wrapper {
-  display: flex;
-  flex-direction: column;
-  max-height: 100vh;
-  height: 100%;
-  width: 100%;
-}
-.title {
-  text-align: center;
-  padding-top: 3rem;
-  padding-bottom: 1rem;
-  h1 {
-    font-size: 2rem;
+<script>
+import Split from '~/components/SplitHalf.vue'
+export default {
+  components: {
+    Split
   }
 }
-.text {
-  overflow: scroll;
-  padding: 2rem 3rem;
-  flex: 1;
-  font-size: 1.5rem;
-  line-height: 2rem;
-}
+</script>
 
-@media (min-width: 700px) {
-  .wrapper {
-    flex-direction: row;
-  }
-  .title{
-    padding: 0;
-    width: 40%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding-bottom: 3rem;
-  }
-  .text {
-    font-size: 1rem;
-      letter-spacing: 0.1rem;
-  }
-}
-</style>
 
