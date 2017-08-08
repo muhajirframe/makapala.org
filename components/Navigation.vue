f7d0b769dfbfda92a3dc0aa40b1e2e2ba4452910
Muhammad Muhajir <muhajirframe@gmail.com>
2017-08-08 12:37:36 +0700

    add translateY animation to Navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -73,14 +73,13 @@ export default {
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity .5s
+  transition: transform .5s ease, opacity .5s ease;
 }
 
 .fade-enter,
 .fade-leave-to
-/* .fade-leave-active below version 2.1.8 */
-
 {
-  opacity: 0
+  opacity: 0;
+  transform: translateY(10px)
 }
 </style>
